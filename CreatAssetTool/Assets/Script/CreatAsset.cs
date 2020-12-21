9c628fe962a1079bcd94dc71d9653b44f64a33e6
ZLY <1738242284@qq.com>
2020-12-21 19:27:09 +0800

    add creatAssetTool项目

---
+++ CreatAssetTool/Assets/Script/CreatAsset.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.IO;
+using System;
+
+public class CreatAsset : Editor
+{ 
+    public static void Create(string objectName)
+    { 
+        Type type = Type.GetType(ScriptableObject.CreateInstance(typeof(MyBaseAsset)).GetType().Namespace + "." + objectName, true, true);
+        ScriptableObject scriptable = ScriptableObject.CreateInstance(type);
+        if (scriptable==null)
+        {
+            Debug.LogWarning("SpriteAsset is null!");
+            return;
+        } 
+        // 自定义资源保存路径
+        string path = Application.dataPath + "/MyAssets";
+        // 如果项目总不包含该路径，创建一个 
+        if (!Directory.Exists(path))
+        {
+            Directory.CreateDirectory(path); 
+        }
+      
+        path = string.Format("Assets/MyAssets/{0}.asset", objectName.ToString());
+        // 生成自定义资源到指定路径
+        AssetDatabase.CreateAsset(scriptable, path);
+    }
+} 
---
+++ CreatAssetTool/Assets/Script/EXEditorPanel.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using System.IO;
+using System;
+
+[CustomEditor(typeof(EditorPanel))]
+public class EditorPanel : EditorWindow
+{ 
+    [MenuItem("MyTools/CreatAsset")]
+    public static void ConfigDialog()
+    { 
+        EditorWindow.GetWindow(typeof(EditorPanel));
+    } 
+    public UnityEngine.Object go = null; 
+    void OnGUI()
+    {
+        //Label
+        GUILayout.Label("CreatAsset", EditorStyles.boldLabel); 
+        go = EditorGUILayout.ObjectField(go, typeof(UnityEngine.Object), true);
+        //Button
+        if (GUILayout.Button("Creat Asset"))
+        {
+            string className = go.name;
+            CreatAsset.Create(className);
+            go = null;
+        }
+    }
+}
